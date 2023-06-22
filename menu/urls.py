7f07334051a40e38335ffcad6904a97399d9f671
Shakil Islam <shakilislamtoha@gmail.com>
2023-06-22 12:33:17 +0600

    menu list updating

--- menu/urls.py
+++ menu/urls.py
@@ -2,11 +2,13 @@ from django.urls import (path, include)
 from . views import (home, service, menu_view_2, menu_view_3, about)
 
 urlpatterns = [
-    path('home', home, name='home'),
-    path('service', service, name='service'),
-    path('about', about, name='about'),
-    path('de', menu_view_2, name='design'),
-    path('service', menu_view_3, name='service'),
-    path('service', menu_view_3, name='html'),
-    path('service', menu_view_3, name='develop')
+    path('', home, name='home'),
+    path('services', service, name='service'),
+    path('abouts', about, name='about'),
+    path('services/design', menu_view_2, name='design'),
+    path('services/design/html', menu_view_3, name='html'),
+    path('services/develop', menu_view_3, name='develop'),
+    path('services/graphic', menu_view_3, name='graphic'),
+    path('services/graphic/logo', menu_view_3, name='logo'),
+    path('services/graphic/banner', menu_view_3, name='banner'),
 ]
